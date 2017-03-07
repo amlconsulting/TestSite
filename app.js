c6cc1cf72246b743aa6e4757727f223de0e40994
Alex <amlane86@yahoo.com>
2017-03-07 10:56:53 -0500

    Initial upload

---
+++ app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(request, response) {
+   response.send('build/index';) 
+});
---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    plumber = require('gulp-plumber'),
+    sass = require('gulp-ruby-sass'),
+    ts = require('gulp-typescript'),
+    browserSync = require('browser-sync').create();
+
+//Compile and Minify SASS/CSS
+gulp.task('compile-minify-scss', function () {
+    return sass('app/scss/**/*.scss', {
+        style: 'compressed'
+    })
+    .pipe(gulp.dest('build/Release/css'))
+    .pipe(browserSync.stream());
+});
+
+//Minify JavaScript
+gulp.task('minify-js', function () {
+    gulp.src('app/js/**/*.js')
+        .pipe(plumber())
+        .pipe(uglify())
+        .pipe(gulp.dest('build/Release/js'))
+        .pipe(browserSync.stream());
+});
+
+//Compile TypeScript
+gulp.task('compile-typescript', function () {
+    gulp.src('app/typescript/**/*.ts')
+        .pipe(ts({
+            out: 'output.js'
+        }))
+    .pipe(gulp.dest('build/Release/js'))
+    .pipe(browserSync.stream());
+});
+
+//Copy Pages
+gulp.task('copy-pages', function () {
+	gulp.src('app/pages/**/*html')
+		.pipe(gulp.dest('build/Release');
+});
+
+//Serve the content
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./build/Release"
+        }
+    });
+});
+
+//Watch and Serve
+gulp.task('watch', ['browser-sync'], function () {
+    gulp.watch('app/scss/**/*.scss', ['compile-minify-scss']);
+    gulp.watch('app/js/**/*.js', ['minify-js']);
+    gulp.watch('app/typescript/**/*.ts', ['compile-typescript']);
+    gulp.watch('app/pages/**/*.html', ['copy-pages']).on('change', browserSync.reload);
+});
+
+gulp.task('default', [
+    'compile-minify-scss', 
+    'minify-js', 
+    'compile-typescript', 
+	'copy-pages',
+    'watch'
+]);
