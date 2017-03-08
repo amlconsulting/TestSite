8401e19cc2917522724f43a2df72d6394563aeb3
Alex <amlane86@yahoo.com>
2017-03-08 09:06:55 -0500

    Connected to MongoDB with authentication and selected from collection.

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 'use strict';
 
-var dotenv = require('dotenv').config();
+require('dotenv').config();
 
 var mongo = require('mongodb').MongoClient,
     assert = require('assert');
 
-var url = 'mongodb://' + process.env.DATABASE_HOST + ':' + process.env.DATABASE_PORT + '/' + process.env.DATABASE_NAME;
+var url = `mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`;
 
 console.log("Connecting to " + url);
 
 mongo.connect(url, function(err, db) {
     assert.equal(null, err);
     console.log("Connected successfully to server");
-    
+
+    var users = db.collection('users').find();
+
+    users.forEach(function (user) {
+        console.log(`First name: ${user.first_name}, Last name: ${user.last_name}`);
+    });
+
     db.close();
 });
